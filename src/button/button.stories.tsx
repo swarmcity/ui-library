05f80d6f2e328074b995bae422907916175eeb97
Barbara Gomes <barbara@brickriver.com>
2022-06-20 21:11:43 -0400

    Updated button component

--- src/button/button.stories.tsx
+++ src/button/button.stories.tsx
@@ -3,29 +3,77 @@ import { Button } from './button'
 import type { Meta, Story } from '@storybook/preact'
 
 export default {
-	title: 'Components/Button',
+	title: "Components/Button",
 	component: Button,
 	argTypes: {
+		bgColor: {
+			control: "select",
+			options: ["default", "light", "danger", "success", "primary"],
+			default: "default",
+		},
 		color: {
-			control: 'select',
-			options: ['default', 'red', 'green', 'black', 'white'],
-			default: 'default',
+			control: "select",
+			options: ["default", "primary", "dark", "success", "danger", "light"],
+			default: "default",
 		},
 		size: {
-			control: 'radio',
-			options: ['default', 'large'],
-			default: 'default',
+			control: "radio",
+			options: ["default", "large"],
+			default: "default",
 		},
 		disabled: {
-			control: 'boolean',
+			control: "boolean",
 			default: false,
 		},
 	},
-} as Meta<typeof Button>
+} as Meta<typeof Button>;
 
 const Template: Story<typeof Button> = (args) => <Button {...args} />
 
-export const Primary = Template.bind({})
+export const Default = Template.bind({});
+Default.args = {
+	children: "Default Button",
+};
+
+export const DefaultSuccess = Template.bind({});
+DefaultSuccess.args = {
+	children: "Default Success Button",
+	bgColor: "bgLight",
+	color: "success",
+};
+
+export const DefaultDark = Template.bind({});
+DefaultDark.args = {
+	children: "Default Dark Button",
+	bgColor: "bgLight",
+	color: "dark",
+};
+
+export const DefaultDanger = Template.bind({});
+DefaultDanger.args = {
+	children: "Default Danger Button",
+	bgColor: "bgLight",
+	color: "danger",
+};
+
+
+export const Primary = Template.bind({});
 Primary.args = {
-	children: 'Primary button',
-}
+	children: "Primary button",
+	bgColor: "primary",
+	color: "light",
+};
+
+export const Success = Template.bind({});
+Success.args = {
+	children: "Success button",
+	bgColor: "success",
+	color: "light",
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+	children: "Danger button",
+	bgColor: "danger",
+	color: "light",
+};
--- src/button/button.tsx
+++ src/button/button.tsx
@@ -2,23 +2,33 @@ import { h } from 'preact'
 import cn from 'classnames'
 import classes from './button.module.css'
 
+
+const BGCOLORS = {
+	danger: classes.bgDanger,
+	success: classes.bgSuccess,
+	primary: classes.bgPrimary,
+	light: classes.bgLight,
+};
+
 const COLORS = {
-	red: classes.red,
-	green: classes.green,
-	black: classes.black,
-	white: classes.white,
-}
+	danger: classes.danger,
+	success: classes.success,
+	dark: classes.dark,
+	light: classes.light,
+};
 
 const SIZE = {
 	large: classes.large,
 }
 
 export type ButtonProps = h.JSX.HTMLAttributes<HTMLButtonElement> & {
-	color?: keyof typeof COLORS
-	size?: keyof typeof SIZE
-}
+	bgColor?: keyof typeof BGCOLORS;
+	color?: keyof typeof COLORS;
+	size?: keyof typeof SIZE;
+};
 
 export const Button = ({
+	bgColor,
 	color,
 	size,
 	disabled,
@@ -28,6 +38,7 @@ export const Button = ({
 	<button
 		class={cn(
 			classes.div,
+			bgColor && BGCOLORS[bgColor],
 			color && COLORS[color],
 			size && SIZE[size],
 			disabled && classes.disabled,
@@ -35,4 +46,4 @@ export const Button = ({
 		)}
 		{...props}
 	/>
-)
+);
