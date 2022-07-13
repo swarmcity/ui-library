4290aae4dc1c2f9d8385ee74b18123d73c2c8439
Bargo <sugarpusher@users.noreply.github.com>
2022-07-13 07:43:18 -0400

    feat: add icon button component (#4)

---
+++ src/icon-button/icon-button.stories.tsx
@@ -0,0 +1,51 @@
+import { h } from 'preact'
+import { IconButton } from './icon-button'
+import type { Meta, Story } from '@storybook/preact'
+
+export default {
+	title: 'Components/IconButton',
+	component: IconButton,
+	argTypes: {
+		variant: {
+			name: 'Variant',
+			control: 'select',
+			options: [
+				'addImage',
+				'back',
+				'cancel',
+				'cancelCreation',
+				'chat',
+				'close',
+				'collapse',
+				'errorNext',
+				'conflictNext',
+				'confirmAction',
+				'confirmDanger',
+				'confirmSend',
+				'counter',
+				'exit',
+				'reply',
+				'requestNext',
+				'requestStart',
+				'rotateImg',
+				'select',
+				'upload',
+			],
+			defaultValue: 'close',
+		},
+		disabled: {
+			name: 'Disable',
+			control: 'boolean',
+			defaultValue: false,
+		},
+		open: {
+			name: 'Toggle (collapse only)',
+			control: 'boolean',
+			defaultValue: false,
+		},
+	},
+} as Meta<typeof IconButton>
+
+const Template: Story<typeof IconButton> = (args) => <IconButton {...args} />
+
+export const Default = Template.bind({})
---
+++ src/icon-button/icon-button.tsx
@@ -0,0 +1,51 @@
+import { h } from 'preact'
+import cn from 'classnames'
+import classes from './icon-button.module.css'
+
+const VARIANTS = {
+	cancelCreation: classes.cancelCreation,
+	close: classes.close,
+	cancel: classes.cancel,
+	reply: classes.reply,
+	rotateImg: classes.rotateImg,
+	exit: classes.exit,
+	back: classes.back,
+	collapse: classes.collapse,
+	select: classes.select,
+	conflictNext: classes.conflictNext,
+	requestNext: classes.requestNext,
+	errorNext: classes.errorNext,
+	chat: classes.chat,
+	requestStart: classes.requestStart,
+	confirmAction: classes.confirmAction,
+	confirmDanger: classes.confirmDanger,
+	confirmSend: classes.confirmSend,
+	upload: classes.upload,
+	counter: classes.counter,
+	addImage: classes.addImage,
+}
+
+export type ButtonProps = h.JSX.HTMLAttributes<HTMLButtonElement> & {
+	variant?: keyof typeof VARIANTS
+	disabled?: boolean
+	open?: boolean
+}
+
+export const IconButton = ({
+	variant,
+	open,
+	disabled,
+	class: className,
+	...props
+}: ButtonProps) => (
+	<button
+		class={cn(
+			classes.icon,
+			variant && VARIANTS[variant],
+			open && classes.open,
+			disabled && classes.disabled,
+			className
+		)}
+		{...props}
+	/>
+)
