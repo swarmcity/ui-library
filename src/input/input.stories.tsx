b8090358eefd02c882111ca416a85edb4c4ddc5f
Philippe Schommers <philippe@schommers.be>
2022-07-13 15:45:14 +0200

    feat: add input component (#9)
    * feat: add input component
    
    * chore: add filled-in example

---
+++ src/input/input.stories.tsx
@@ -0,0 +1,32 @@
+import { h } from 'preact'
+import { Input } from './input'
+import type { Meta, Story } from '@storybook/preact'
+
+export default {
+	title: 'Components/Input',
+	component: Input,
+	argTypes: {
+		id: {
+			name: 'id',
+			control: 'text',
+			default: false,
+		},
+		children: {
+			name: 'Label',
+			control: 'text',
+		},
+	},
+} as Meta<typeof Input>
+
+const Template: Story<typeof Input> = (args) => <Input id="id" {...args} />
+
+export const Empty = Template.bind({})
+Empty.args = {
+	children: 'Label',
+}
+
+export const WithValue = Template.bind({})
+WithValue.args = {
+	children: 'Label',
+	value: 'Some value',
+}
