991381f59db0328925cd03b7ba33a81323a5f8e9
Philippe Schommers <philippe@schommers.be>
2022-07-18 14:33:56 +0200

    chore: export all components

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+// Components
 export { Button } from './button/button'
+export { FullscreenLoading } from './fullscreen-loading/fullscreen-loading'
+export { IconButton } from './icon-button/icon-button'
+export { Input } from './input/input'
+
+// Types
+export type { ButtonProps } from './button/button'
+export type { FullscreenLoadingProps } from './fullscreen-loading/fullscreen-loading'
+export type { IconButtonProps } from './icon-button/icon-button'
+export type { InputProps } from './input/input'
