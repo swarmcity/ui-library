ff2638844b70995a36f65b5ce242442e564d38d4
Barbara Gomes <barbara@brickriver.com>
2022-07-21 10:26:04 -0400

    chore: export requester info component

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ export { Button } from './button/button'
 export { FullscreenLoading } from './fullscreen-loading/fullscreen-loading'
 export { IconButton } from './icon-button/icon-button'
 export { Input } from './input/input'
+export { RequesterInfo } from './requester-info/requester-info'
 
 // Types
 export type { ButtonProps } from './button/button'
 export type { FullscreenLoadingProps } from './fullscreen-loading/fullscreen-loading'
 export type { IconButtonProps } from './icon-button/icon-button'
 export type { InputProps } from './input/input'
+export type { RequesterInfoProps } from './requester-info/requester-info'
