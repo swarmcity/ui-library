7f1e616b389cb562ccf4b9c0e318b6378a7171cb
Bargo <sugarpusher@users.noreply.github.com>
2022-07-22 09:47:49 -0400

    feat: requester info component (#20)

---
+++ src/requester-info/requester-info.stories.tsx
@@ -0,0 +1,38 @@
+import { h } from 'preact'
+import { RequesterInfo } from './requester-info'
+import type { Meta, Story } from '@storybook/preact'
+
+export default {
+	title: 'Components/Requester Info',
+	component: RequesterInfo,
+	argTypes: {
+		requesterName: {
+			name: 'Requester Name',
+			type: { name: 'string', required: true },
+			defaultValue: 'Sally Singer',
+			default: 'Sally Singer',
+		},
+		requesterRep: {
+			name: 'Requester Reputation',
+			type: { name: 'number', required: true },
+			defaultValue: 52,
+			default: 52,
+		},
+		myRequest: {
+			name: 'My Request?',
+			control: 'boolean',
+			defaultValue: false,
+		},
+		detail: {
+			name: 'Detail?',
+			control: 'boolean',
+			defaultValue: false,
+		},
+	},
+} as Meta<typeof RequesterInfo>
+
+const Template: Story<typeof RequesterInfo> = (args) => (
+	<RequesterInfo requesterName="Sally Singer" requesterRep={52} {...args} />
+)
+
+export const Default = Template.bind({})
---
+++ src/requester-info/requester-info.tsx
@@ -0,0 +1,31 @@
+import { h } from 'preact'
+import cn from 'classnames'
+import classes from './requester-info.module.css'
+import defaultAvatar from './defaultAvatar.jpg'
+
+export type RequesterInfoProps = h.JSX.HTMLAttributes<HTMLDivElement> & {
+	requesterName: string
+	requesterRep: number
+	myRequest?: boolean
+	avatar?: string
+	detail?: boolean
+}
+
+export const RequesterInfo = ({
+	class: className,
+	...props
+}: RequesterInfoProps) => (
+	<div
+		class={cn(
+			classes.requesterInfo,
+			props.detail && classes.detail,
+			props.myRequest && classes.myRequest,
+			className
+		)}
+	>
+		<img src={props.avatar ?? defaultAvatar} />
+		<span>{props.requesterName}</span>
+		<span class={cn(classes.dot, props.myRequest && classes.myDot)} />
+		<span>{props.requesterRep} SWR</span>
+	</div>
+)
