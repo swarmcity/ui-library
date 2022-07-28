ef5425a618a02e4ac50dfb6306efbaf52940c74b
Bargo <sugarpusher@users.noreply.github.com>
2022-07-28 12:34:17 -0400

    Update index.ts
    Add Card, ConfirmModal, Reply components

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 // Components
 export { Button } from './button/button'
+export { Card } from './card/card'
+export { ConfirmModal } from './confirm-modal/confirm-modal'
 export { FullscreenLoading } from './fullscreen-loading/fullscreen-loading'
 export { IconButton } from './icon-button/icon-button'
 export { Input } from './input/input'
 export { MarketplaceItem } from './marketplace-item/marketplace-item'
+export { Reply } from './reply/reply'
 export { RequestItem } from './request-item/request-item'
 export { UserInfo } from './user-info/user-info'
 
 // Types
 export type { ButtonProps } from './button/button'
+export type { CardProps } from './card/card'
+export type { ConfirmModalProps } from './confirm-modal/confirm-modal'
 export type { FullscreenLoadingProps } from './fullscreen-loading/fullscreen-loading'
 export type { IconButtonProps } from './icon-button/icon-button'
 export type { InputProps } from './input/input'
 export type { MarketplaceItemProps } from './marketplace-item/marketplace-item'
+export type { ReplyProps } from './reply/reply'
 export type { RequestItemProps } from './request-item/request-item'
 export type { UserInfoProps } from './user-info/user-info'
