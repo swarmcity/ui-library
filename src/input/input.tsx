4ea0f4cc6052085a4b90496e0e559e07cf5e3ee4
Vojtech Simetka <vojtech@simetka.cz>
2022-10-25 21:37:31 +0200

    feat: create forwarding ref for input field

--- src/input/input.tsx
+++ src/input/input.tsx
@@ -1,4 +1,4 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from 'react'
+import { DetailedHTMLProps, InputHTMLAttributes, forwardRef } from 'react'
 
 // Style
 import classes from './input.module.css'
@@ -10,9 +10,11 @@ export type InputProps = DetailedHTMLProps<
 	id: string
 }
 
-export const Input = ({ id, children, ...props }: InputProps) => (
-	<div className={classes.inputGroup}>
-		<input id={id} required {...props} />
-		<label htmlFor={id}>{children}</label>
-	</div>
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+	({ id, children, ...props }, ref) => (
+		<div className={classes.inputGroup}>
+			<input id={id} required {...props} ref={ref} />
+			<label htmlFor={id}>{children}</label>
+		</div>
+	)
 )
